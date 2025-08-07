391210bf4b92e4c07c6dfea0e2c455ff89e892f4
fjarmdhn <mfajarramadhan211@gmail.com>
2025-08-07 09:43:05 +0700

    first commit

---
+++ app/Http/Controllers/TesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tes;
+use App\Http\Requests\StoreTesRequest;
+use App\Http\Requests\UpdateTesRequest;
+
+class TesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tes $tes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tes $tes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTesRequest $request, Tes $tes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tes $tes)
+    {
+        //
+    }
+}
